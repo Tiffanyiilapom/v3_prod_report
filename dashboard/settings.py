53e8962edb91c8fd4913ec77146278264ee62a33
Tiffanyiilapom <chenyuchen2001@gapp.nthu.edu.tw>
2024-07-19 14:02:20 +0800

    localhost

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#0=o8%02z05jc@rn^%5hptxm6ii!!lz*9dl#r_wobbj9s6&jw3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
