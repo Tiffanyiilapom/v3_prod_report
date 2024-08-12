d9f3ce27c2c9b48fa688a811771a21ca6cb3258f
Tiffanyiilapom <chenyuchen2001@gapp.nthu.edu.tw>
2024-08-12 16:33:17 +0800

    頁次切換 (second level)

--- EOL_RT/views.py
+++ EOL_RT/views.py
@@ -19,6 +19,20 @@ from plotly.subplots import make_subplots
 
 dash = My_Dash()
 
+def func_for_pie(pie_data, title):
+    labels = pie_data['labels'].tolist()
+    values = pie_data['values'].tolist()
+
+    fig = go.Figure(data=[go.Pie(
+        labels=labels,
+        values=values,
+        rotation=300  # Rotates the pie chart slices
+    )])
+
+    fig.update_layout(title=title)
+    fig_html = pio.to_html(fig, full_html=False)
+    return fig_html
+
 def for_error(request):
     # 把舊的東西清掉!
     dash.data = None 
@@ -29,7 +43,34 @@ def for_error(request):
     return render(request, "ERROR_Page.html")
 
 def daily(request):
-    return render(request,'EOL_RT_p1.html')
+    try:
+        selected_tab = request.GET.get('tab', 'ATE')
+        if selected_tab == 'OPT':
+            chart_data = {
+                'labels': ['RUN', 'FAI', 'IDLE', 'ENG', 'DOWN', 'PM', 'HOLD_M'],
+                'values': [996, 13, 21, 24, 4, 7, 18]
+            }
+            title = '2024-08-12 EOL : OPT Production Time Distribution'
+        else:
+            chart_data = {
+                'labels': ['RUN', 'FAI', 'IDLE', 'WAIT_M', 'FAC', 'PM', 'HOLD_M'],
+                'values': [985, 13, 21, 32, 4, 7, 58]
+            }
+            title = '2024-08-12 EOL : ATE Production Time Distribution'
+
+        pie_data = pd.DataFrame(chart_data)
+        placeholder_fig = func_for_pie(pie_data, title)
+        
+    except :
+        placeholder_fig = dash.placeholder_figure()
+        placeholder_fig = placeholder_fig.to_html(full_html=False, default_height=500, default_width=1200)
+
+    context = {
+        'placeholder_fig': placeholder_fig,
+    }
+
+    return render(request, 'EOL_RT_p1.html', context)
+
 
 def weekly(request):
     return render(request,'EOL_RT_p2.html')
